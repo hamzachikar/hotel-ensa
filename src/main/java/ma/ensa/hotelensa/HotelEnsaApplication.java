8c0ae59ae3e856e9cd5de163f5a4ab08f7a9aed9
hamzachikar <chikar.h@outlook.fr>
2020-12-12 14:38:32 +0100

    init

---
+++ src/main/java/ma/ensa/hotelensa/HotelEnsaApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.hotelensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelEnsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelEnsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ensa/hotelensa/HotelEnsaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.hotelensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelEnsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
