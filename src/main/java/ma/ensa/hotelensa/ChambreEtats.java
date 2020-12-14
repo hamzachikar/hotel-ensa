f0533a2dd184ea4e018645fe04ee1b6f0137907d
Aatafay Mohamed Jawad <mohamedjawad.aatafay@gmail.com>
2020-12-14 16:06:59 +0100

    added chambre functions

---
+++ src/main/java/ma/ensa/hotelensa/ChambreEtats.java
@@ -0,0 +1,5 @@
+package ma.ensa.hotelensa;
+
+public enum ChambreEtats {
+    DISPONIBLE,OCCUPE,MAINTENANCE
+}
--- src/main/java/ma/ensa/hotelensa/rest/ChambreRestController.java
+++ src/main/java/ma/ensa/hotelensa/rest/ChambreRestController.java
@@ -1,7 +1,33 @@
 package ma.ensa.hotelensa.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import ma.ensa.hotelensa.ChambreEtats;
+import ma.ensa.hotelensa.beans.Chambre;
+import ma.ensa.hotelensa.services.IChambreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/chambres")
 public class ChambreRestController {
+
+    @Autowired
+    IChambreService chambreService;
+
+    @GetMapping(path = "/{etat}")
+    public ResponseEntity<List<Chambre>> getAvailableChambers(@PathVariable(value = "etat") ChambreEtats etat) {
+        return ResponseEntity.ok(
+                chambreService.getChambersByEtat(etat)
+        );
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Chambre> updateEmployee(@PathVariable(value = "id") int chambreId,
+                                                  @RequestBody ChambreEtats etat) {
+        return ResponseEntity.ok(
+                chambreService.modifierEtatChambre(chambreId, etat)
+        );
+    }
 }
