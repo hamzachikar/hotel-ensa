82e9239c6fe49870ba1d5b798dda64c5fb9d5630
hamzachikar <chikar.h@outlook.fr>
2021-01-03 21:19:22 +0100

    sonar

--- src/main/java/ma/ensa/hotelensa/HotelEnsaApplication.java
+++ src/main/java/ma/ensa/hotelensa/HotelEnsaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelEnsaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelEnsaApplication.class, args);
+		 SpringApplication.run(HotelEnsaApplication.class, args);
 	}
 
 }
